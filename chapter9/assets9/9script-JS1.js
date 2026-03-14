4ad2c15b56f6fa61c5eb0961e3bb7610d2be9e56
DustinGann <123513919+DustinGann@users.noreply.github.com>
2026-03-14 01:41:57 +0000

    ll

--- chapter9/assets9/9script-JS1.js
+++ chapter9/assets9/9script-JS1.js
@@ -7,23 +7,26 @@ $(document).ready( () => {
 		$(h2).toggleClass("minus");
 
 		if ($(h2).attr("class") != "minus") {
-		   	$(h2).next().hide();
+			$(h2).next().fadeOut(300);
+		   
 	   	}
 	   	else {
-	        $(h2).next().show();
+
+	        $(h2).next().show(500);
+			
 		   }
 		   
 		evt.preventDefault();
     }); // end click
     
     // runs when the page is ready
-    $("#faqs h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 1000 )  
-		         .animate( { fontSize: "175%", left: "-=200" }, 1000 );
+    $("#faqs h1").animate( { fontSize: "175%", opacity: 1, left: "+=375" }, 1000, "easeInExpo")  
+		         .animate( {  left: "0" }, 1000 ,"easeOutBounce");
 		    
 	// runs when the top-level heading is clicked
 	$("#faqs h1").click( () => {
 		$("#faqs h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 2000 )  
-			         .animate( { fontSize: "175%", left: "-=200" }, 1000 );
+			         .animate( { fontSize: "175%", left: "0" }, 1000 );
 	}); // end click
     
 }); // end ready
